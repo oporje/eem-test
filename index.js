81c650047fbcb6b1edf0d4c008bd33b13f3f1d7e
Hung Truong <hungtruongquoc@gmail.com>
2018-02-23 09:33:02 -0600

    Added new package and test getting files

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
+// telling *nix systems that the interpreter of our JavaScript file should be
+// /usr/bin/env node which looks up for the locally-installed node executable
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+const glob = require('glob');
 
 clear();
 console.log(
@@ -9,3 +14,7 @@ console.log(
     figlet.textSync('EEM Test', { horizontalLayout: 'full' })
   )
 );
+
+glob('**/*.spec.ts', {}, (er, files) => {
+  console.log(files);
+});
