cd5951c7bfe57b44e4f88f340c5e0493cdb7af1d
Hung Truong <hungtruongquoc@gmail.com>
2018-02-23 09:06:14 -0600

    Added code for testing es 6 in the project

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('EEM Test', { horizontalLayout: 'full' })
+  )
+);
